98b251a81103bde5d4cb61cb3a5be34bdadd7f70
hanlee0707 <han.lee0707@gmail.com>
2013-04-29 12:30:57 +0900

    word count should work now.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,26 @@ Alphanumeric::Application.configure do
   config.serve_static_assets = true
 
   config.assets.precompile += %w(ckeditor/init.js)
+  config.assets.precompile << Proc.new { |path|
+    if path =~ /\.(css|js)\z/
+      full_path = Rails.application.assets.resolve(path).to_path
+      app_assets_path = Rails.root.join('app', 'assets').to_path
+      vendor_assets_path = Rails.root.join('vendor', 'assets').to_path
+      
+      if ((full_path.starts_with? app_assets_path) || (full_path.starts_with? vendor_assets_path)) && (!path.starts_with? '_')
+        puts "\t" + full_path.slice(Rails.root.to_path.size..-1)
+        true
+      else
+        false
+      end
+    else
+      false
+    end
+  }
+
   # Compress JavaScripts and CSS
+
+
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
