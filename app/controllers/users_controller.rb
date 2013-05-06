fff7ba3d884cba0b5647d0175352209650cce5ea
hanlee0707 <han.lee0707@gmail.com>
2013-05-07 02:31:20 +0900

    level 2 admin can delete users now.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,12 @@ class UsersController < ApplicationController
   before_filter :logged_in?, :only => [:edit, :update] 
 
   def index
+
     @user_index_layout = true
     if employee_privilege("Administrator") or employee_privilege("Editor") then
+      if current_employee.employee_positions.find_by_position("Administrator").level > 1 then
+        @can_delete = true
+      end
       @users = User.order("last_name").paginate(:per_page => 10, :page => params[:page])
     else
       respond_to do |format|
@@ -49,6 +53,27 @@ class UsersController < ApplicationController
         @back_path = user_home_path
         format.html { render action: "edit" }
         format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    begin
+      if employee_privilege("Administrator") or employee_privilege("Editor") then
+        @user = User.find(params[:id])
+        @user.destroy    
+        respond_to do |format|
+          format.html { redirect_to staff_users_path, notice: "The user was successfully deleted." }
+          format.json { head :no_content }
+        end
+      else
+        respond_to do |format|
+          format.html { redirect_to home_path, notice: "You don't have the required privilege." }
+        end
+      end
+    rescue ActiveRecord::RecordNotFound
+      respond_to do |format|
+        format.html { redirect_to employees_path, notice: "That employee does not exist."}
       end
     end
   end
